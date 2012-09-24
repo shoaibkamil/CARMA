5c0571b88a3d868bf25a606eb880734f479edc2e
Shoaib Kamil <skamil@sandy.Millennium.Berkeley.EDU>
2012-09-24 05:04:46 -0700

    vtune_multiply now uses PAPI instead of vtune.

--- vtune_multiply.c
+++ vtune_multiply.c
@@ -1,4 +1,5 @@
 #include "header.h"
+#include <papi.h>
 
 void initialize(int m, int k, int n, float* A, float* B, float* C);
 void multiply(int m, int k, int n, float *A, float *B, float *C);
@@ -12,9 +13,23 @@ int main(int argc, char **argv) {
   float *B = (float*) malloc(k * n * sizeof(float));
   float *C = (float*) malloc(m * n * sizeof(float));
 
+  int Events[] = {PAPI_FP_INS, PAPI_TOT_CYC};
+  long_long values[2];
+#define NUM_EVENTS 2
+
   initialize(m, k, n, A, B, C);
 
+  /* Start counting events */
+  if (PAPI_start_counters(Events, NUM_EVENTS) != PAPI_OK)
+	  exit(10);
+
   multiply(m, k, n, A, B, C);
+
+  /* Stop counting events */
+  if (PAPI_stop_counters(values, NUM_EVENTS) != PAPI_OK)
+	  exit(10);
+
+  printf("Counter values: %ld, %ld\n", values[0], values[1]);
 
   free(A);
   free(B);
