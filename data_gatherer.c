d138f18adbc6ce125971b7c544a583a89df6a326
Shoaib Kamil <skamil@sandy.Millennium.Berkeley.EDU>
2012-09-26 10:54:03 -0700

    Now we make sure the timing is takes >0.1 seconds before doing multiply, and use multiple matrices to meet the threshold.

--- data_gatherer.c
+++ data_gatherer.c
@@ -3,6 +3,8 @@
 void initialize(int m, int k, int n, double* A, double* B, double* C);
 void multiply(int m, int k, int n, double *A, double *B, double *C);
 
+#define NUM_SMALL_MATRICES_MAX 1000
+
 void clearCache(double *F) {
   int i;
   double sum = 0;
@@ -12,6 +14,45 @@ void clearCache(double *F) {
   if (sum == 0.1) { // Prevent the compiler from optimizing this away
     printf("sum = %f\n", sum);
   }
+}
+
+int guess_num_matrices(int m, int k, int n) {
+  double *A[NUM_SMALL_MATRICES_MAX], *B[NUM_SMALL_MATRICES_MAX], *C[NUM_SMALL_MATRICES_MAX];
+  double *cacheClearer = (double*) malloc(100000000); //L3 cahce is less than 100MB
+  int num_matrices = 1;
+  struct timeval start, end;
+  int i,j;
+
+//  printf("Starting loop...\n");
+
+  for (i=0; i<NUM_SMALL_MATRICES_MAX; i++) {
+//	printf("Loop iteration %d\n", i);
+	num_matrices = i+1;
+	A[i] = (double*) malloc(m*k*n*sizeof(double));
+	B[i] = (double*) malloc(m*k*n*sizeof(double));
+	C[i] = (double*) malloc(m*k*n*sizeof(double));
+//	printf("Malloc done.\n");
+	initialize(m, k, n, A[i], B[i], C[i]);
+
+//	printf("Initialize done.\n");
+
+    clearCache(cacheClearer); // clear cache
+	gettimeofday(&start, NULL);
+	for (j=0; j<=i; j++) {
+		  multiply(m, k, n, A[j], B[j], C[j]);
+	  }
+	gettimeofday(&end, NULL);
+	double seconds = (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+	//printf("%i matrices take %f seconds.\n", num_matrices, seconds);
+	if (seconds > 0.1)
+		break;
+  }
+
+  for (i=0; i<num_matrices; i++)
+	  free(A[i]);
+
+  return num_matrices;
+
 }
 
 int main(int argc, char **argv) {
@@ -28,21 +69,31 @@ int main(int argc, char **argv) {
 
   FILE *f = fopen("data.csv","a");
 
-  double *A = (double*) malloc(m * k * sizeof(double));
-  double *B = (double*) malloc(k * n * sizeof(double));
-  double *C = (double*) malloc(m * n * sizeof(double));
+  double *A[NUM_SMALL_MATRICES_MAX], *B[NUM_SMALL_MATRICES_MAX], *C[NUM_SMALL_MATRICES_MAX];
 
-  initialize(m, k, n, A, B, C);
+  // discover how many multiplies are needed
+  int num_matrices = guess_num_matrices(m, k, n);
+  printf("Num matrices required: %d\n", num_matrices);
+
+
+  for (i=0; i<num_matrices; i++) {
+	  A[i] = (double*) malloc(m * k * sizeof(double));
+	  B[i] = (double*) malloc(k * n * sizeof(double));
+	  C[i] = (double*) malloc(m * n * sizeof(double));
+
+	  initialize(m, k, n, A[i], B[i], C[i]);
+	}
 
   // Time multiplication
   struct timeval start, end;
-  multiply(m, k, n, A, B, C); // warmup
+  multiply(m, k, n, A[0], B[0], C[0]); // warmup
   clearCache(cacheClearer); // clear cache
   gettimeofday(&start, NULL);
-  multiply(m, k, n, A, B, C);
+  for (i=0; i<num_matrices; i++) multiply(m, k, n, A[i], B[i], C[i]);
   gettimeofday(&end, NULL);
+  // SK: I'm leaving seconds calculation the same, but calculating GFlop/s by scaling by num_matrices
   double seconds = (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
-  double Gflop_s = 2e-9 * m * k * n / seconds;
+  double Gflop_s = num_matrices * 2e-9 * m * k * n / seconds;
 
   if (seconds < 0.01) {
     printf("WARNING: Matrix size may be too small to produce accurate timing data\n");
@@ -68,9 +119,9 @@ int main(int argc, char **argv) {
   }
   */
 
-  free(A);
-  free(B);
-  free(C);
+  for (i=0; i<num_matrices; i++) free(A[i]);
+  for (i=0; i<num_matrices; i++) free(B[i]);
+  for (i=0; i<num_matrices; i++) free(C[i]);
   free(cacheClearer);
   fclose(f);
   return 0;
